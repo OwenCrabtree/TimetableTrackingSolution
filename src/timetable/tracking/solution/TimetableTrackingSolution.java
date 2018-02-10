62f9aad0c242863ff0250a40a46b2dd73b7a70e5
Boris <b.figeczky@ayhoo.com>
2018-02-10 11:10:33 +0000

    first commit

---
+++ src/timetable/tracking/solution/TimetableTrackingSolution.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package timetable.tracking.solution;
+
+/**
+ *
+ * @author Bobo
+ */
+public class TimetableTrackingSolution {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          // Declares and creates a new instance of GaaTicketsApplicationGUI
+        LoginForm newLoginForm = new LoginForm ();
+        
+        // Sets the GUI to be visible to the user
+        newLoginForm .setVisible(true);
+        
+        // Sets the GUI to the center of the screen once the code is executed
+        newLoginForm .setLocationRelativeTo(null);
+    }
+    
+}
